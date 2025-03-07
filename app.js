e05d2563a71277dbd83a47b1c07339b9fda8c24a
Iheagwaram <tiheagwaram5@gmail.com>
2025-03-07 10:07:35 +0100

    joydas backend deployable

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { connectDB } = require("./config/db");
 const app = express();
 const useRoute = require("./route/auth/auth")
-const PORT = 5000;
+const PORT = "5000";
 
 connectDB();
 
 app.use(express.json());
 
-app.use('/api', useRoute);
+app.use('/api/signin', useRoute);
+
+app.use('/api/signup', useRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const userSchema = mongoose.Schema({
     firstName : {type : String, required: true},
     middleName: {type: String, required : true},
+    gender: {type: String, required : true},
     email: {type: String, required: true},
     password: {type: String, required: true},
     // createdAt: {type: Date, default: Date.now},
